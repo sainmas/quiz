5db6b57aa87f1cc337803d4b358f2a554c686913
Mason Sain <sainmason012@gmail.com>
2024-05-02 13:49:53 -0400

    Added survey page

--- index.php
+++ index.php
@@ -8,6 +8,7 @@ error_reporting(E_ALL);
 
 // Require autoload.php
 require_once ('vendor/autoload.php');
+require_once ('model/data-layer.php');
 
 // Instantiate the F3 base class
 $f3 = Base::instance();
@@ -19,6 +20,44 @@ $f3->route('GET /', function() {
     $view = new Template();
     echo $view->render('views/home.html');
 });
+
+// Define the survey route
+$f3->route('GET|POST /survey', function($f3) {
+
+    //Add the variables
+    $name = "";
+    $checkboxes = "";
+
+    //If the page has been POSTed
+    if($_SERVER['REQUEST_METHOD'] == "POST") {
+
+        if (isset($_POST['name'])) {
+            $name = $_POST['name'];
+        }
+        if (isset($_POST['checks'])) {
+            $checkboxes = $_POST['checks'];
+        }
+
+        //Add the data to the session array
+        $f3->set('SESSION.name', $name);
+        $f3->set('SESSION.checks', $checkboxes);
+
+        // Reroute to summary page
+        $f3->reroute('summary');
+    }
+
+    // Get the data from the model
+    // add it to the F3 hive
+    $checks = GetChecks();
+    $f3->set('checks', $checks);
+
+    // Render a view page
+    $view = new Template();
+    echo $view->render('views/survey.html');
+});
+
+
+
 
 // Run Fat-Free
 $f3->run();
---
+++ model/data-layer.php
@@ -0,0 +1,11 @@
+<?php
+
+/* This is my data layer
+ * This belongs to the model
+ */
+
+// Gets the checkboxes for the survey
+function GetChecks()
+{
+    return array('This midterm is ez', 'I REALLY like midterms', 'Today is Thursday');
+}
